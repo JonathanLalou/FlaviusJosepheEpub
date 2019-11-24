3038250972789cad7dcae583379df4fddc2b7f59
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2019-11-23 23:56:43 -0500

    appended timestamp on generated file name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf *.epub
 cd src
-zip -ll -r ../La_Guerre_des_Judeens-Flavius_Josephe-FR-GR-2019.epub .
+zip -ll -r ../La_Guerre_des_Judeens-Flavius_Josephe-FR-GR-2019-`date +%Y%m%d-%H%M`.epub .
 cd ..
