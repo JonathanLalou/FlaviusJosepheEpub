86c020e9927c363966f45f0da9ccffbff4dffb74
Jonathan Lalou <Jonathan.Lalou@gmail.com>
2020-02-23 01:08:56 -0500

    fixed build script finally got a valid epub validation!!! ``` Validating using EPUB version 2.0.1 rules. No errors or warnings detected. Messages: 0 fatals / 0 errors / 0 warnings / 0 infos
    EPUBCheck completed
    ```

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 rm -rf *.epub
 cd src
-zip -ll -r ../La_Guerre_des_Judeens-Flavius_Josephe-FR-GR-2019-`date +%Y%m%d-%H%M`.epub .
+zipDate=`date +%Y%m%d-%H%M`
+# -0: store without compression
+# -X: don't add extra attributes (such as date)
+# -r: recursive (useless on a single file)
+zip -0 -X -r ../La_Guerre_des_Judeens-Flavius_Josephe-FR-GR-2019-$zipDate.epub mimetype
+# -ll: convert LF to CR LF (-ll CR LF to LF)
+#  -9: compress better
+zip -9 -ll -r ../La_Guerre_des_Judeens-Flavius_Josephe-FR-GR-2019-$zipDate.epub META-INF OEBPS
 cd ..
